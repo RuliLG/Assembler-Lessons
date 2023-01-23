2e45150f62e1da4b72b8769348783ac146acf807
Raúl López <hi@raullg.com>
2023-01-23 11:04:14 +0000

    Initial commit

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::whereHas('posts')->get();
+        return response()->json([
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'unique:categories,name'],
+        ]);
+        $category = new Category();
+        $category->name = $request->name;
+        $category->save();
+        return response()->json([
+            'category' => $category,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return response()->json([
+            'category' => $category,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'unique:categories,name,' . $category->id],
+        ]);
+
+        $category->name = $request->name;
+        $category->save();
+        return response()->json([
+            'category' => $category,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json([
+            'success' => true,
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\PostsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::apiResource('categories', CategoriesController::class);
+Route::apiResource('posts', PostsController::class);
+Route::get('categories/{category}/posts', [PostsController::class, 'postsByCategory']);
